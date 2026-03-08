67ff0861f717284eb802f7054d13a7a207669c9e
ursfrndlyshiva <141220456+ursfrndlyshiva@users.noreply.github.com>
2026-03-08 16:16:28 +0530

    Delete react-native-task-widget directory

--- react-native-task-widget/android/gradle.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-REM Convenience wrapper requested by user; forwards to Gradle wrapper.
-set DIR=%~dp0
-if exist "%DIR%gradlew.bat" (
-  call "%DIR%gradlew.bat" %*
-) else (
-  echo gradlew.bat not found. Run "npm run prebuild" first.
-  exit /b 1
-)
